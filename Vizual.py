672276658ac97123b51081b5519cebd59421028d
Vishal Dubey <vishdube1221@gmail.com>
2026-02-23 19:52:32 +0530

    Hello

---
+++ Vizual.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+# Step 1: Data prepare karo
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+# Step 2: Plot banao
+plt.plot(x, y)
+# Step 3: Title aur labels add karo
+plt.title('Mera Pehla Graph')
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+# Step 4: Graph dikhao
+plt.show()
